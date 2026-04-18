e4857115207b9614f161dad6c553a85969f8eceb
Vladimir Kolchurin <18503099+kolchurinvv@users.noreply.github.com>
2026-04-18 19:36:25 +0200

    correct usage of the bun adapter

--- frontend/server.ts
+++
@@ -1,8 +0,0 @@
-import { handle } from "./.svelte-kit/adapter-bun/index.js";
-
-const server = Bun.serve({
-  port: 3000,
-  fetch: (req) => handle(req),
-});
-
-console.log(`Server running at http://localhost:${server.port}`);
